d2db2c9d41728c18b2665b4f5bcd96daacfc03e1
Godstime Osarobo <nosarobo@gmail.com>
2018-11-14 13:35:29 +0000

    created angular 7 project and added bootstrap

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: { title: 'Home' },
+  },
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: '/home',
+  },
+];
+@NgModule({
+  declarations: [AppComponent, HomeComponent, NavbarComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
