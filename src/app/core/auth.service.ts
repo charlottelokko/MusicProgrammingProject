9dc0517240479178c0b1807eb1e188f9fb8e91be
ejab271 <ejab271@gmail.com>
2018-12-14 17:24:51 +0000

    got the login/logout in working properly

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -63,7 +63,8 @@ export class AuthService {
         uid: user.uid,
         email: user.email,
         displayName: user.displayName,
-        photoURL: user.photoURL
+        photoURL: user.photoURL,
+      
       }
   
       return userRef.set(data, { merge: true })
