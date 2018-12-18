7a7087b0782535b5e3e1683f05d63b1a33706a1f
Charlotte Lokko <littlelokko@hotmail.com>
2018-12-18 19:21:02 +0000

    Updating search. Added loop through the returned tracks to get artists, name, image etc... Working on fixing search

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -28,8 +28,54 @@ export class NavbarComponent implements OnInit {
     if (this.searchStr !== '') {
       this._spotifyService.searchMusic(this.searchStr).subscribe(
         res => {
-          // this gets the data instead using .json()
           console.log((res as any).tracks.items);
+          // console.log((res as any).tracks.items.length);
+          // console.log((res as any).tracks.items[0].artists.length);
+          const trackAmount = (res as any).tracks.items.length;
+          let artists;
+          let track;
+          let image;
+          let trackId;
+          const element = document.getElementById('drop');
+
+          element.innerHTML = ' ';
+          for (let i = 0; i < 10; i++) {
+            trackId = (res as any).tracks.items[i].id;
+            console.log((res as any).tracks.items[i].id);
+            artists = ' ';
+            const artistsAmount = (res as any).tracks.items[i].artists.length;
+            console.log((res as any).tracks.items[i].name);
+            track = (res as any).tracks.items[i].name;
+            console.log((res as any).tracks.items[i].album.images[1].url);
+
+            for (let j = 0; j < artistsAmount; j++) {
+              console.log((res as any).tracks.items[i].artists[j].name);
+              if(j>0){
+              artists += ((res as any).tracks.items[i].artists[j].name + ', ');
+              }
+              else{
+                artists = (res as any).tracks.items[i].artists[j].name;
+              }
+            }
+            const divy = document.createElement('div');
+            const atag = document.createElement('a');
+            const breaky = document.createElement('br');
+
+            // const trackList = document.createTextNode(track + ' : ');
+            // const artistList = document.createTextNode(artists);
+            // atag.appendChild(trackList);
+            // atag.appendChild(artistList);
+            atag.innerHTML = (track + ' : ' + artists);
+            atag.setAttribute("href", "main#" +  trackId);
+            atag.setAttribute("href", "main#" +  trackId);
+
+            // divy.appendChild(atag);
+
+            element.appendChild(atag);
+            element.appendChild(breaky);
+          }
+          // console.log((res as any).tracks.items[0].name);
+          // console.log((res as any).tracks.items[0].artists[0].name);
         },
         err => {
           console.log('Error:' + err);
@@ -38,3 +84,4 @@ export class NavbarComponent implements OnInit {
     }
   }
 }
+
