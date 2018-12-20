6286c8fbd151f6382fb25290edd79ca069f43ab0
Godstime Osarobo <nosarobo@gmail.com>
2018-12-20 15:06:48 +0000

    fixed minor problems

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -13,32 +13,24 @@ import * as $ from 'jquery';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   albumsobj: Array<any>;
   constructor(private _spotifyService: SpotifyService) {
-
-      $(document).ready(() => {
-        setTimeout(() => {
-            this._spotifyService.displayPlaylist().subscribe(
-                      res => {
-                    this.albumsobj = (res as any).tracks.items;
-                    console.log(this.albumsobj);
-                   
-                        
-                      });
-        }, 3000)
-       
+    $(document).ready(() => {
+      const displayplaylist = this._spotifyService
+        .displayPlaylist()
+        .subscribe(res => {
+          this.albumsobj = (res as any).tracks.items;
+          console.log(this.albumsobj);
+        });
     });
-   }
-
-  ngOnInit() {
   }
+
+  ngOnInit() {}
 
   homeView() {
-    console.log("hello");
-
+    console.log('hello');
   }
-
 }
