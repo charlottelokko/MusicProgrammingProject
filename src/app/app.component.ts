b4b1a371e63f0baa6300904710d5f0fad4a3b084
Godstime Osarobo <nosarobo@gmail.com>
2018-12-20 12:31:57 +0000

    made content on homepage justified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,8 @@ import {
   Route,
   Router,
   RouterState
-} from '@angular/router'
+} from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -16,7 +17,39 @@ import {
 })
 export class AppComponent {
   title = 'MusicProgrammingProject';
+  constructor(
+    private router: Router,
+    private activatedRoute: ActivatedRoute,
+    private titleService: Title
+  ) {
+    router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        const title = this.getTitle(
+          router.routerState,
+          router.routerState.root
+        ).join('-');
+        console.log('title', title);
+        this.title = title;
+        titleService.setTitle(title + ' | Purpose Movement');
+      }
+    });
+  }
+  // collect the title data properties from all child routes
+  // there might be a better way but this worked for me
+  getTitle(state: RouterState, parent: ActivatedRoute) {
+    const data = [];
+    if (parent && parent.snapshot.data && parent.snapshot.data.title) {
+      data.push(parent.snapshot.data.title);
+    }
 
-  constructor() {
+    if (state && parent) {
+      data.push(...this.getTitle(state, parent.firstChild));
+    }
+
+    return data;
+  }
+
+  public setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle);
   }
 }
